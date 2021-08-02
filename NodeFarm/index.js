4dbf7b7d1b2e9e1a24d384a2f862cdc3788e62b0
Oleksandr Bohatko <alexbogatko1@gmail.com>
2021-08-02 22:39:09 +0300

    added overview page

--- NodeFarm/index.js
+++ NodeFarm/index.js
@@ -1,59 +1,61 @@
-const fs = require('fs') 
+const fs = require('fs')
 const http = require('http')
-const { dirname } = require('path/posix')
 const url = require('url')
-
-
-///////////////////////////////////////
-// FILES
-
-//block
-// const text = fs.readFileSync('./txt/input.txt','utf-8')
-// console.log(text);
-
-// const txtOut = `This is what we know about avocado: ${text}.\nCreated on ${Date.now()}`
-// fs.writeFileSync('./txt/output.txt',txtOut)
-// console.log("File Written!");
-
-//nonblock
-// fs.readFile('./txt/start.txt','utf-8', (err,data1)=>{
-//     fs.readFile(`./txt/${data1}.txt`,'utf-8', (err,data2)=>{
-//         console.log(data2);
-//         fs.readFile('./txt/append.txt','utf-8', (err,data3)=>{
-//             console.log(data3);
-
-//             fs.writeFile('./txt/final.txt',`${data2}\n${data3}`,'utf-8',err=>{
-//                 console.log('File has been written !!');
-//             })
-//         })
-//     })
-// })
-// console.log('Will read file!');
 
 ///////////////////////////////////////
 // SERVER
+
+const replaceTemplate = (temp, product) => {
+    let out = temp.replace(/{%PRODUCTNAME%}/g, product.productName)
+    out = out.replace(/{%IMAGE%}/g, product.image)
+    out = out.replace(/{%PRICE%}/g, product.price)
+    out = out.replace(/{%FROM%}/g, product.from)
+    out = out.replace(/{%NUTRIENTS%}/g, product.nutrients)
+    out = out.replace(/{%QUANTITY%}/g, product.quantity)
+    out = out.replace(/{%ID%}/g, product.id)
+
+    if (!product.organic) out = out.replace(/{%NOT_ORGANIC%}/g, 'not-organic')
+
+    return out;
+}
+const tempOverview = fs.readFileSync(`${__dirname}/templates/template-overview.html`, 'utf-8')
+const tempCard = fs.readFileSync(`${__dirname}/templates/template-card.html`, 'utf-8')
+const tempProduct = fs.readFileSync(`${__dirname}/templates/template-product.html`, 'utf-8')
+
 const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8')
 const dataObj = JSON.parse(data)
 
 
-const server = http.createServer((req, res)=>{
+
+const server = http.createServer((req, res) => {
     const pathname = req.url
-    if(pathname === '/' || pathname === '/overview'){
-        res.end("This is OVERVIEW")
-    }else if(pathname === '/product'){
+
+    //Overview page
+    if (pathname === '/' || pathname === '/overview') {
+        res.writeHead(200, { 'Content-type': 'text/html' })
+        const cardsHtml = dataObj.map(el => replaceTemplate(tempCard, el)).join('')
+        const output = tempOverview.replace('{%PRODUCT_CARDS%}', cardsHtml)        
+        res.end(output)
+
+        //Product page
+    } else if (pathname === '/product') {
         res.end('This is PRODUCT')
-    }else if (pathname === '/api'){
-        res.writeHead(200, {'Content-type':'application/json'})
+
+        //API
+    } else if (pathname === '/api') {
+        res.writeHead(200, { 'Content-type': 'application/json' })
         res.end(data)
-        }else{
+
+        //Not found
+    } else {
         res.writeHead(404, {
-            'Content-type':'text/html',
-            'my-own-header':'hello-world'
+            'Content-type': 'text/html',
+            'my-own-header': 'hello-world'
         })
         res.end('<h1>Page could not be found!\nerror 404</h1>')
     }
 })
 
-server.listen(8000,"127.0.0.1", ()=>{
+server.listen(8000, "127.0.0.1", () => {
     console.log("listen to server");
 })
