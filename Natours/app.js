c0d0b7638bb63f2073e21825cb4ecb132e7b64f4
Oleksandr Bohatko <alexbogatko1@gmail.com>
2021-08-06 17:58:40 +0300

    logger + middleware

--- Natours/app.js
+++ Natours/app.js
@@ -1,20 +1,34 @@
 const express = require('express');
 const fs = require('fs');
+const morgan = require('morgan')
 
-// Init express
 const app = express();
 
+
 // Middleware
+app.use(morgan('dev'))
 app.use(express.json());
+
+app.use((req,res,next)=>{
+    console.log('Hello middleware');
+    next()
+})
+app.use((req,res,next)=>{
+    req.requestTime = new Date().toISOString()
+    next()
+})
 
 // Get Tours
 const tours = JSON.parse(
   fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
 );
 
+// Route handlers
 const getAllTours = (req, res) => {
+    console.log(req.requestTime);
   res.status(200).json({
     status: 'success',
+    requestAt:req.requestTime,
     results: tours.length,
     data: {
       tours: tours,
@@ -85,10 +99,13 @@ const updateTour = (req, res) => {
     },
   });
 };
+
+// Routes
 app
   .route('/api/v1/tours')
   .get(getAllTours)
   .post(createTour);
+
 
 app
   .route('/api/v1/tours/:id')
