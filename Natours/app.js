dd924f9a6a99edf25f5ab66590c192b19582405a
Oleksandr Bohatko <alexbogatko1@gmail.com>
2021-08-06 16:28:10 +0300

    get by id

--- Natours/app.js
+++ Natours/app.js
@@ -12,6 +12,7 @@ const tours = JSON.parse(
   fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
 );
 
+// Get all tours
 app.get('/api/v1/tours', (req, res) => {
   res.status(200).json({
     status: 'success',
@@ -22,6 +23,27 @@ app.get('/api/v1/tours', (req, res) => {
   });
 });
 
+//Get tour by id
+app.get('/api/v1/tours/:id', (req, res) => {
+  const id = req.params.id * 1;
+  const tour = tours.find((el) => el.id === id);
+
+  if (!tour) {
+    return res.status(404).json({
+      status: 'fail',
+      message: 'Invalid ID',
+    });
+  }
+
+  res.status(200).json({
+    status: 'success',
+    data: {
+      tours: tour,
+    },
+  });
+});
+
+//Create new tour
 app.post('/api/v1/tours', (req, res) => {
   const newId = tours[tours.length - 1].id + 1;
   const newTour = Object.assign({ id: newId }, req.body);
