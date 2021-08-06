4e22f19e2a064b750d44d4ba7888020594daa22e
Oleksandr Bohatko <alexbogatko1@gmail.com>
2021-08-06 16:06:59 +0300

    Tours GET & POST

--- Natours/app.js
+++ Natours/app.js
@@ -1,13 +1,48 @@
-const express = require('express')
+const express = require('express');
+const fs = require('fs');
 
+// Init express
 const app = express();
 
-app.get('/',(req,res)=>{
-    res.send()
-})
+// Middleware
+app.use(express.json());
+
+// Get Tours
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
+);
+
+app.get('/api/v1/tours', (req, res) => {
+  res.status(200).json({
+    status: 'success',
+    results: tours.length,
+    data: {
+      tours: tours,
+    },
+  });
+});
+
+app.post('/api/v1/tours', (req, res) => {
+  const newId = tours[tours.length - 1].id + 1;
+  const newTour = Object.assign({ id: newId }, req.body);
+
+  tours.push(newTour);
+  fs.writeFile(
+    `${__dirname}/dev-data/data/tours-simple.json`,
+    JSON.stringify(tours),
+    (err) => {
+      res.status(201).json({
+        status: 'success',
+        data: {
+          tour: newTour,
+        },
+      });
+    }
+  );
+});
 
 const port = 3000;
 
-app.listen(port,()=>{
-    console.log(`App running on port ${port}...`);
-})
+app.listen(port, () => {
+  console.log(`App running on port ${port}...`);
+});
