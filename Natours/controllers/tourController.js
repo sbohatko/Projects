112d43fd788fd62c6fec96d8c020d0cd384ada2c
Oleksandr Bohatko <alexbogatko1@gmail.com>
2021-08-09 19:52:59 +0300

    GET filtering added, fix logging

--- Natours/controllers/tourController.js
+++ Natours/controllers/tourController.js
@@ -2,8 +2,17 @@ const Tour = require('../models/tourModel');
 
 const getAllTours = async (req, res) => {
   try {
-    const tours = await Tour.find();
+    // BUILD QUERY
+    const queryObj = { ...req.query };
+    const excludedFields = ['page', 'sort', 'limit', 'fields'];
+    excludedFields.forEach((el) => delete queryObj[el]);
 
+    const query = Tour.find(queryObj);
+
+    // EXECUTE QUERY
+    const tours = await query;
+
+    // SEND RESPONSE
     res.status(200).json({
       status: 'success',
       results: tours.length,
--- Natours/routes/userRoutes.js
+++ Natours/routes/userRoutes.js
@@ -1,15 +1,17 @@
-const express = require('express')
-const userController = require('./../controllers/userController')
+const express = require('express');
+const userController = require('../controllers/userController');
+
 const router = express.Router();
 
-router.route('/')
+router
+  .route('/')
   .get(userController.getAllUsers)
   .post(userController.createUser);
 
-  router
+router
   .route('/:id')
   .get(userController.getUser)
   .patch(userController.updateUser)
   .delete(userController.deleteUser);
 
-  module.exports = router
+module.exports = router;
