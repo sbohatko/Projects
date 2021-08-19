a5796daa22886f709825575667209e1920cdccc4
Oleksandr Bohatko <alexbogatko1@gmail.com>
2021-08-19 23:25:40 +0300

    JWT + Login user

--- Natours/controllers/authController.js
+++ Natours/controllers/authController.js
@@ -1,16 +1,45 @@
+const jwt = require('jsonwebtoken');
 const User = require('../models/userModel');
+const AppError = require('../utils/appError');
 const catchAsync = require('../utils/catchAsync');
 
 const signup = catchAsync(async (req, res, next) => {
-  const newUser = await User.create(req.body);
+  const newUser = await User.create({
+    name: req.body.name,
+    email: req.body.email,
+    password: req.body.password,
+    passwordConfirm: req.body.passwordConfirm,
+  });
+
+  const token = jwt.sign({ id: newUser._id }, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_EXPIRES_IN,
+  });
+
   res.status(201).json({
     status: 'success',
+    token,
     data: {
       user: newUser,
     },
   });
 });
 
+const login = (req, res, next) => {
+  const { email, password } = req.body;
+  // 1) Check if email and password exists
+  if (!email || !password) {
+    next(new AppError('Please provide email and password'), 400);
+  }
+  // 2) Check if user exists && password is correct
+
+  // 3) If everything is ok - send token to client
+  const token = '';
+  res.status(200).json({
+    status: 'success',
+    token,
+  });
+};
 module.exports = {
   signup,
+  login,
 };
