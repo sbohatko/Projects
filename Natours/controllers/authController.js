5c1cab8adf2034051ed12a488775279a9f386550
Oleksandr Bohatko <alexbogatko1@gmail.com>
2021-08-20 00:22:41 +0300

    'protect' middleware for routes

--- Natours/controllers/authController.js
+++ Natours/controllers/authController.js
@@ -46,7 +46,32 @@ const login = catchAsync(async (req, res, next) => {
     token,
   });
 });
+
+const protect = catchAsync(async (req, res, next) => {
+  // 1) Get token and check if it`s there
+  let token;
+  if (
+    req.headers.authorization &&
+    req.headers.authorization.startsWith('Bearer')
+  ) {
+    token = req.headers.authorization.split(' ')[1];
+  }
+
+  if (!token) {
+    return next(
+      new AppError('You are not logged in! Please log in to get access', 401)
+    );
+  }
+  // 2) Verification token
+
+  // 3) Check if user still exists
+
+  // 4) Check if user changed password after the token was issued
+
+  next();
+});
 module.exports = {
+  protect,
   signup,
   login,
 };
--- Natours/routes/tourRoutes.js
+++ Natours/routes/tourRoutes.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const tourController = require('../controllers/tourController');
+const authController = require('../controllers/authController');
 
 const router = express.Router();
 
@@ -14,7 +15,7 @@ router.route('/mouthly-plan/:year').get(tourController.getMouthlyPlan);
 
 router
   .route('/')
-  .get(tourController.getAllTours)
+  .get(authController.protect, tourController.getAllTours)
   .post(tourController.createTour);
 
 router
