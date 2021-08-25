6b80085d5b80c0fd29908e0746ba96af480e8091
Oleksandr Bohatko <alexbogatko1@gmail.com>
2021-08-26 00:14:15 +0300

    review routes added

---
+++ Natours/controllers/reviewController.js
@@ -0,0 +1,33 @@
+const Review = require('../models/reviewModel');
+const catchAsync = require('../utils/catchAsync');
+const APIFeatures = require('../utils/apiFeatures');
+
+const getAllReviews = catchAsync(async (req, res, next) => {
+  // EXECUTE QUERY
+  const features = new APIFeatures(Review.find(), req.query)
+    .filter()
+    .sort()
+    .limitFields()
+    .paginate();
+
+  const reviews = await features.query;
+  res.status(200).json({
+    status: 'success',
+    results: reviews.length,
+    data: { reviews },
+  });
+});
+const createReview = catchAsync(async (req, res, next) => {
+  const newReview = await Review.create(req.body);
+  res.status(201).json({
+    status: 'success',
+    data: {
+      review: newReview,
+    },
+  });
+});
+
+module.exports = {
+  getAllReviews,
+  createReview,
+};
