76028e0b709b854675cd2c234acba90c69c25b62
Oleksandr Bohatko <alexbogatko1@gmail.com>
2021-08-27 14:18:19 +0300

    nested for GET tour review

--- Natours/controllers/reviewController.js
+++ Natours/controllers/reviewController.js
@@ -3,14 +3,10 @@ const catchAsync = require('../utils/catchAsync');
 const APIFeatures = require('../utils/apiFeatures');
 
 const getAllReviews = catchAsync(async (req, res, next) => {
-  // EXECUTE QUERY
-  const features = new APIFeatures(Review.find(), req.query)
-    .filter()
-    .sort()
-    .limitFields()
-    .paginate();
+  let filter = {};
+  if (req.params.tourId) filter = { tour: req.params.tourId };
 
-  const reviews = await features.query;
+  const reviews = await Review.find(filter);
   res.status(200).json({
     status: 'success',
     results: reviews.length,
