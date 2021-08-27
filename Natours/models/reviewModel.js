6c650209ed6ae419ee146b189e67a8243886965f
Oleksandr Bohatko <alexbogatko1@gmail.com>
2021-08-27 23:35:13 +0300

    calculate avg rating and quantity on Tour

--- Natours/models/reviewModel.js
+++ Natours/models/reviewModel.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const Tour = require('./tourModel');
 
 //review / rating /createdAt / ref to tour / ref to user
 const reviewSchema = new mongoose.Schema(
@@ -41,5 +42,41 @@ reviewSchema.pre(/^find/, function (next) {
   next();
 });
 
+reviewSchema.statics.calcAverageRating = async function (tourId) {
+  const stats = await this.aggregate([
+    { $match: { tour: tourId } },
+    {
+      $group: {
+        _id: '$tour',
+        nRating: { $sum: 1 },
+        avgRating: { $avg: '$rating' },
+      },
+    },
+  ]);
+  if (stats.length > 0) {
+    await Tour.findByIdAndUpdate(tourId, {
+      ratingQuantity: stats[0].nRating,
+      ratingAverage: stats[0].avgRating,
+    });
+  } else {
+    await Tour.findByIdAndUpdate(tourId, {
+      ratingQuantity: 0,
+      ratingAverage: 4.5,
+    });
+  }
+};
+reviewSchema.post('save', function () {
+  this.constructor.calcAverageRating(this.tour);
+});
+reviewSchema.pre(/^findOneAnd/, async function (next) {
+  this.r = await this.findOne();
+  next();
+});
+reviewSchema.post(/^findOneAnd/, async function (next) {
+  // await this.findOne(); does NOT work here, query has already executed
+  await this.r.constructor.calcAverageRating(this.r.tour);
+});
+
 const Review = mongoose.model('Review', reviewSchema);
+
 module.exports = Review;
