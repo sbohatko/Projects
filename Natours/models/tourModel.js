a07e9e98496aae6699a8bb6ea57307fbdd462309
Oleksandr Bohatko <alexbogatko1@gmail.com>
2021-08-28 00:54:09 +0300

    Get Distances to Tours From Point

--- Natours/models/tourModel.js
+++ Natours/models/tourModel.js
@@ -153,10 +153,10 @@ tourSchema.post(/^find/, function (docs, next) {
 });
 
 // AGGREGATION MIDDLEWARE
-tourSchema.pre('aggregate', function (next) {
-  this.pipeline().unshift({ $match: { secretTour: { $ne: true } } });
-  next();
-});
+// tourSchema.pre('aggregate', function (next) {
+//   this.pipeline().unshift({ $match: { secretTour: { $ne: true } } });
+//   next();
+// });
 
 const Tour = mongoose.model('Tour', tourSchema);
 
--- Natours/routes/tourRoutes.js
+++ Natours/routes/tourRoutes.js
@@ -35,6 +35,8 @@ router
   .route('/tours-within/:distance/center/:latlng/unit/:unit')
   .get(tourController.getToursWithin);
 
+router.route('/distances/:latlng/unit/:unit').get(tourController.getDistances);
+
 router
   .route('/:id')
   .get(tourController.getTour)
