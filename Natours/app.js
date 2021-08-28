afac03d2f6ebea3adfe7c7474946777ca55af443
Oleksandr Bohatko <alexbogatko1@gmail.com>
2021-08-28 17:54:50 +0300

    extends Pug templates

--- Natours/app.js
+++ Natours/app.js
@@ -70,7 +70,20 @@ app.use((req, res, next) => {
 
 // Routes
 app.get('/', (req, res) => {
-  res.status(200).render('base');
+  res.status(200).render('base', {
+    tour: 'The Forest Hiker',
+    user: 'Jonas',
+  });
+});
+app.get('/overview', (req, res) => {
+  res.status(200).render('overview', {
+    title: 'All Tours',
+  });
+});
+app.get('/tour', (req, res) => {
+  res.status(200).render('tour', {
+    title: 'The Forest Hiker Tour',
+  });
 });
 
 app.use('/api/v1/tours', tourRouter);
