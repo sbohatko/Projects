86ca6c07f6346ecf162c3e23b5f82cba802f919d
Oleksandr Bohatko <alexbogatko1@gmail.com>
2021-08-29 00:12:14 +0300

    tour details page

--- Natours/controllers/viewsController.js
+++ Natours/controllers/viewsController.js
@@ -13,8 +13,16 @@ const getOverview = catchAsync(async (req, res) => {
   });
 });
 const getTour = catchAsync(async (req, res) => {
+  // 1) get the data for the requested tour including reviews and guides
+  const tour = await Tour.findOne({ slug: req.params.slug }).populate({
+    path: 'reviews',
+    fields: 'review rating user',
+  });
+  // 2)Build template
+  //3) render template using the data from step 1
   res.status(200).render('tour', {
-    title: 'The Forest Hiker Tour',
+    title: tour.name,
+    tour,
   });
 });
 module.exports = {
