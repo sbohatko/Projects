8c5deeb062c1ccf3f620652cef01340db01aa76c
Oleksandr Bohatko <alexbogatko1@gmail.com>
2021-08-29 01:40:33 +0300

    mapbox added

--- Natours/controllers/viewsController.js
+++ Natours/controllers/viewsController.js
@@ -20,10 +20,16 @@ const getTour = catchAsync(async (req, res) => {
   });
   // 2)Build template
   //3) render template using the data from step 1
-  res.status(200).render('tour', {
-    title: tour.name,
-    tour,
-  });
+  res
+    .status(200)
+    .set(
+      'Content-Security-Policy',
+      "default-src 'self' https://*.mapbox.com ;base-uri 'self';block-all-mixed-content;font-src 'self' https: data:;frame-ancestors 'self';img-src 'self' data:;object-src 'none';script-src https://cdnjs.cloudflare.com https://api.mapbox.com 'self' blob: ;script-src-attr 'none';style-src 'self' https: 'unsafe-inline';upgrade-insecure-requests;"
+    )
+    .render('tour', {
+      title: `${tour.name} Tour`,
+      tour,
+    });
 });
 module.exports = {
   getOverview,
