f075a218118238b2044cf0f89f1fc3fe2f6bd364
Oleksandr Bohatko <alexbogatko1@gmail.com>
2021-08-30 00:05:20 +0300

    setup login page

--- Natours/controllers/viewsController.js
+++ Natours/controllers/viewsController.js
@@ -1,7 +1,7 @@
 const Tour = require('../models/tourModel');
 const catchAsync = require('../utils/catchAsync');
 
-const getOverview = catchAsync(async (req, res) => {
+const getOverview = catchAsync(async (req, res, next) => {
   // 1) Get tour data from colelction
   const tours = await Tour.find();
   // 2) Build template
@@ -12,7 +12,7 @@ const getOverview = catchAsync(async (req, res) => {
     tours,
   });
 });
-const getTour = catchAsync(async (req, res) => {
+const getTour = catchAsync(async (req, res, next) => {
   // 1) get the data for the requested tour including reviews and guides
   const tour = await Tour.findOne({ slug: req.params.slug }).populate({
     path: 'reviews',
@@ -31,7 +31,19 @@ const getTour = catchAsync(async (req, res) => {
       tour,
     });
 });
+const getLoginForm = (req, res) => {
+  res
+    .status(200)
+    .set(
+      'Content-Security-Policy',
+      "default-src 'self' https://*.mapbox.com ;base-uri 'self';block-all-mixed-content;font-src 'self' https: data:;frame-ancestors 'self';img-src 'self' data:;object-src 'none';script-src https://cdnjs.cloudflare.com https://api.mapbox.com 'self' blob: ;script-src-attr 'none';style-src 'self' https: 'unsafe-inline';upgrade-insecure-requests;"
+    )
+    .render('login', {
+      title: 'Log into your account',
+    });
+};
 module.exports = {
+  getLoginForm,
   getOverview,
   getTour,
 };
